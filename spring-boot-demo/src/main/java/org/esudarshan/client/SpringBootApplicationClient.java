328afa79cad75e8a3e427a8709993ef4a0f44a38
esudarshan <sudarshanekambe@gmail.com>
2018-01-21 20:33:05 +0530

    Spring Boot Demo

---
+++ spring-boot-demo/src/main/java/org/esudarshan/client/SpringBootApplicationClient.java
@@ -0,0 +1,13 @@
+package org.esudarshan.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationClient {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationClient.class, args);
+	}
+
+}
