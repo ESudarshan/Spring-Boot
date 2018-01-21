5ac557e131d4612ae9933454fbf430c75a779e63
esudarshan <sudarshanekambe@gmail.com>
2018-01-22 00:09:59 +0530

    Spring Boot Demo

--- spring-boot-demo/src/main/java/org/esudarshan/springbootdemo/controller/UserController.java
+++ spring-boot-demo/src/main/java/org/esudarshan/springbootdemo/controller/UserController.java
@@ -6,6 +6,7 @@ import org.esudarshan.springbootdemo.model.User;
 import org.esudarshan.springbootdemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,4 +27,18 @@ public class UserController {
 		return userService.getUserById(id);
 	}
 
+	@RequestMapping(value = "/users", method = RequestMethod.POST)
+	public User createUser(@RequestBody User user) {
+		return userService.createUser(user);
+	}
+
+	@RequestMapping(value = "/users/{id}", method = RequestMethod.PUT)
+	public User updateUser(@PathVariable int id, @RequestBody User user) {
+		return userService.updateUser(id, user);
+	}
+
+	@RequestMapping(value = "/users/{id}", method = RequestMethod.DELETE)
+	public String deleteUser(@PathVariable int id) {
+		return userService.deleteUser(id);
+	}
 }
--- spring-boot-demo/src/main/java/org/esudarshan/springbootdemo/service/UserService.java
+++ spring-boot-demo/src/main/java/org/esudarshan/springbootdemo/service/UserService.java
@@ -1,5 +1,6 @@
 package org.esudarshan.springbootdemo.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,8 +10,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-	private List<User> users = Arrays.asList(new User(1, "Sudarshan"), new User(2, "Ajinkya"), new User(3, "Ravish"),
-			new User(4, "Manish"));
+	private List<User> users = new ArrayList<>(Arrays.asList(new User(1, "Sudarshan"), new User(2, "Ajinkya"),
+			new User(3, "Ravish"), new User(4, "Manish")));
 
 	public List<User> getAllUsers() {
 		return users;
@@ -18,6 +19,26 @@ public class UserService {
 
 	public User getUserById(int id) {
 		return users.stream().filter(u -> u.getId() == id).findFirst().get();
+	}
+
+	public User createUser(User user) {
+		users.add(user);
+		return user;
+	}
+
+	public User updateUser(int id, User user) {
+		for (int i = 0; i < users.size(); i++) {
+			if (users.get(i).getId() == id) {
+				users.set(i, user);
+				break;
+			}
+		}
+		return user;
+	}
+
+	public String deleteUser(int id) {
+		users.removeIf(u -> u.getId() == id);
+		return "User with id " + id + " is deleted!";
 	}
 
 }
