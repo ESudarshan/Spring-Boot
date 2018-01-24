0ff984e7ca9b0e8b99a25c6ac8b9821db9112555
esudarshan <sudarshanekambe@gmail.com>
2018-01-24 23:36:42 +0530

    Spring Boot Data JPA Demo

---
+++ spring-boot-data-jpa-demo/src/main/java/org/esudarshan/springbootdatajpademo/SpringBootDataJpaDemoApplication.java
@@ -0,0 +1,12 @@
+package org.esudarshan.springbootdatajpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaDemoApplication.class, args);
+	}
+}
